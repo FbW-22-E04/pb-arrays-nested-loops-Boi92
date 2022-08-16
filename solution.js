4e26056eb34faf9a4f7124d8d170a87e2f483e07
Boi Schrader <boi1992@t-online.de>
2022-08-16 16:52:05 +0200

    almost finished

---
+++ solution.js
@@ -0,0 +1,19 @@
+//1
+for (let i = 0; i < 4; i++){
+    let star = "";
+    for (let y = 0; y <= i; y++){
+        star += "*";
+    }
+    console.log(star)
+}
+
+//2
+const ARR = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+for (let i = 0; i < ARR.length; i++){
+    console.log(`row ${i}`);
+    for (let y = 0; y < ARR[i].length; y++){
+        console.log (`${ARR[i][y]}`);
+    }
+}
+
